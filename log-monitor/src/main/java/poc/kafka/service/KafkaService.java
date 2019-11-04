b613fb377f0bea23fbd6c32cd17fc91fb9443342
Ashish Bhosle <ashish.bhosle008@gmail.com>
2019-11-04 15:32:12 +0530

    Initial commit

---
+++ log-monitor/src/main/java/poc/kafka/service/KafkaService.java
@@ -0,0 +1,109 @@
+package poc.kafka.service;
+
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
+import poc.kafka.properties.AppProperties;
+import poc.kafka.properties.KafkaProperties;
+
+/**
+ * @author ashishb888
+ */
+
+@Service
+@Slf4j
+public class KafkaService {
+
+	@Autowired
+	private KafkaProperties kp;
+	@Autowired
+	private AppProperties ap;
+	private ObjectMapper objectMapper = new ObjectMapper();
+	@Autowired
+	public JavaMailSender mailSender;
+
+	private void start() {
+		log.debug("start service");
+
+		Consumer<Integer, String> consumer = consumer();
+		consumer.subscribe(Arrays.asList("filebeat"));
+
+		while (true) {
+			ConsumerRecords<Integer, String> records = consumer.poll(Duration.ofMillis(5));
+
+			for (ConsumerRecord<Integer, String> consumerRecord : records) {
+				// log.debug("consumerRecord: " + consumerRecord.value());
+
+				try {
+					JsonNode root = objectMapper.readTree(consumerRecord.value());
+					String message = root.get("message").asText();
+					JsonNode fields = root.with("fields");
+					String app = fields.get("application").asText().toUpperCase();
+					JsonNode host = root.with("host");
+					String hostname = host.get("hostname").asText().toUpperCase();
+					log.info("message: " + message);
+					log.info("app: " + app);
+					log.info("fields: " + fields);
+					log.info("host: " + host);
+					log.info("hostname: " + hostname);
+
+					sendMail(ap.getMail().get("from"), ap.getMail().get("to"),
+							MessageFormat.format(ap.getMail().get("subject"), app, hostname), message);
+
+				} catch (IOException e) {
+					log.error(e.getMessage(), e);
+				}
+			}
+		}
+	}
+
+	private void sendMail(String from, String to, String subject, String text) {
+		log.debug("sendMail service");
+
+		try {
+			SimpleMailMessage message = new SimpleMailMessage();
+			message.setFrom(from);
+			message.setTo(to);
+			message.setSubject(subject);
+			message.setText(text);
+			mailSender.send(message);
+		} catch (MailException e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+
+	private Consumer<Integer, String> consumer() {
+
+		Properties kafkaProps = new Properties();
+
+		kp.getKafkaConsumer().forEach((k, v) -> {
+			log.debug("k: " + k + ", v: " + v);
+			kafkaProps.put(k, v);
+		});
+
+		return new KafkaConsumer<>(kafkaProps);
+	}
+
+	public void main() {
+		log.debug("main service");
+
+		start();
+	}
+}
